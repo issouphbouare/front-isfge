dd876e5892e830598428d1874007eb3dee489b1e
Mariam9144 <mariamabdoulayemaiga075@gmail.com>
2024-10-01 15:13:03 +0000

    envoi du 01 10 2024

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
@@ -12,7 +13,19 @@ export class MenuComponent {
   isAdmin = false;
   isUser = false;
   username?: string;
-  constructor(private tokenStorageService: TokenStorageService) { }
+  isMenuCollapsed = true;  // Variable to control the menu state
+  constructor(private tokenStorageService: TokenStorageService,
+    private router: Router
+  ) {
+    // Subscribe to router events to close the menu after navigation
+    this.router.events.subscribe(() => {
+      this.isMenuCollapsed = true; // Close the menu after navigation
+    });
+   }
+
+   closeMenu() {
+    this.isMenuCollapsed = true;
+  }
 
   ngOnInit(): void {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
@@ -26,7 +39,7 @@ export class MenuComponent {
       this.username = user.username;
     }
   }
-  
+
 
   logout(): void {
     this.tokenStorageService.signOut();
